ee5a5b995f0cc9ff6b25a421d41349944c389247
allanreym <allan.mariano@live.com>
2016-07-28 12:03:34 -0400

    updated pets.R

--- pets.R
+++ pets.R
@@ -23,98 +23,105 @@ rm(list=ls())
 
 ##Import training data set into pets
 pets <- read.csv("https://raw.githubusercontent.com/allanreym/ShelterAnimal/master/train.csv")
+pets_test_csv <- read.csv("https://raw.githubusercontent.com/allanreym/ShelterAnimal/master/test.csv")
 
-##Summary of pets data set
+##Summary of pets dataset
 summary(pets)
 
-##Keep original of pets data set
+##Keep original of pets dataset
 pets_orig <- pets
 
-##Isolate month value from DateTime
-pets$DateTime_Month <- as.integer(substr(pets$DateTime, 6, 7))
+##Make pets_test_csv same structure as pets
+pets_test_csv$OutcomeType = as.factor(NA)
+pets_test_csv$OutcomeSubtype = as.factor(NA)
+pets_test_csv$ID <- as.factor(pets_test_csv$ID)
+pets_test_csv <- pets_test_csv[,c(1, 2, 3, 9, 10, 4, 5, 6, 7, 8)]
+names(pets_test_csv) <- names(pets)
 
-##Convert all AgeuponOutcome values to years
-TimeUnit <- word(pets$AgeuponOutcome, -1)
-TimeNum <- as.numeric(word(pets$AgeuponOutcome, 1))
-pets$AgeuponOutcome_Years <- ifelse(grepl(pattern = "week", tolower(TimeUnit)), TimeNum / 52,
-                                    ifelse(grepl(pattern = "month", tolower(TimeUnit)), TimeNum / 12,
-                                           ifelse(grepl(pattern = "day", tolower(TimeUnit)), TimeNum / 365,
-                                                  ifelse(grepl(pattern = "year", tolower(TimeUnit)), TimeNum / 1,
-                                                         NA) ) ) )
+##Function to transform datasets
+##Two datasets need to be passed as arguments so that both datasets have same columns
+trans_pets <- function(pets, pets2) {
+  pets_rn <- nrow(pets)
+  pets_all <- rbind(pets, pets2)
+  
+  ##Transform - Isolate month value from DateTime
+  pets_all$DateTime_Month <- as.integer(substr(pets_all$DateTime, 6, 7))
+  
+  ##Transform - Convert all AgeuponOutcome values to years
+  TimeUnit <- word(pets_all$AgeuponOutcome, -1)
+  TimeNum <- as.numeric(word(pets_all$AgeuponOutcome, 1))
+  pets_all$AgeuponOutcome_Years <- ifelse(grepl(pattern = "week", tolower(TimeUnit)), TimeNum / 52,
+                                      ifelse(grepl(pattern = "month", tolower(TimeUnit)), TimeNum / 12,
+                                             ifelse(grepl(pattern = "day", tolower(TimeUnit)), TimeNum / 365,
+                                                    ifelse(grepl(pattern = "year", tolower(TimeUnit)), TimeNum / 1,
+                                                           NA) ) ) )
+  
+  ##Transform - Isolate sex from SexuponOutcome
+  pets_all$SexuponOutcome_Sex <- ifelse(grepl(pattern = " male", tolower(pets_all$SexuponOutcome)), "male",
+                                        ifelse(grepl(pattern = " female", tolower(pets_all$SexuponOutcome)), "female",
+                                               "unknown sex") )
+  
+  ##Transform - Vector of unique sex values
+  SexuponOutcome_Sex_lvl <- sort(unique(pets_all$SexuponOutcome_Sex))
+  
+  ##Transform - Add new data frame columns of unique sex values
+  pets_all <- data.frame(pets_all, do.call(rbind, lapply(pets_all$SexuponOutcome_Sex, function(x) table(factor(x, levels=SexuponOutcome_Sex_lvl)))), stringsAsFactors = FALSE)
+  
+  ##Transform - Isolate neutered from SexuponOutcome
+  pets_all$SexuponOutcome_Neutered <- ifelse(grepl(pattern = "intact", tolower(pets_all$SexuponOutcome)), "intact",
+                                             ifelse(grepl(pattern = "neutered", tolower(pets_all$SexuponOutcome)), "neutered",
+                                                    ifelse(grepl(pattern = "spayed", tolower(pets_all$SexuponOutcome)), "neutered",
+                                                           "unknown neutered") ) )
+  
+  ##Transform - Vector of unique neutered values
+  SexuponOutcome_Neutered_lvl <- sort(unique(pets_all$SexuponOutcome_Neutered))
+  
+  ##Transform - Add new data frame columns of unique neutered values
+  pets_all <- data.frame(pets_all, do.call(rbind, lapply(pets_all$SexuponOutcome_Neutered, function(x) table(factor(x, levels=SexuponOutcome_Neutered_lvl)))), stringsAsFactors = FALSE)
+  
+  ##Transform - Determine and isolate Mix or Pure breed from Breed
+  ##If Breed string contains "mix" or "/", then record will be classified as Mix
+  pets_all$Breed_Mix <- as.integer(grepl("mix|\\/", tolower(pets_all$Breed)))
+  pets_all$Breed_Pure <- as.integer(!pets_all$Breed_Mix)
+  
+  ##Transform - Vector of unique AnimalType values
+  AnimalType_lvl <- sort(unique(pets_all$AnimalType))
+  
+  ##Transform - Add new data frame columns of unique Animal values
+  pets_all <- data.frame(pets_all, do.call(rbind, lapply(pets_all$AnimalType, function(x) table(factor(x, levels=AnimalType_lvl)))), stringsAsFactors = FALSE)
+  
+  ##Transform - From Breed values, remove "mix" pattern and split combination of breeds that use "/"
+  Breed_lst <- strsplit(as.character(sub(" mix", "", tolower(pets_all$Breed))), "/")
+  
+  ##Transform - Vector of unique Breed values
+  Breed_lvl <- sort(unique(unlist(Breed_lst)))
+  
+  ##Transform - Add new data frame columns of unique Breed values
+  pets_all <- data.frame(pets_all, do.call(rbind, lapply(Breed_lst, function(x) table(factor(x, levels=Breed_lvl)))), stringsAsFactors = FALSE)
+  
+  ##Transform - From Color values, split combination of colours that use "/"
+  Color_lst <- strsplit(as.character(tolower(pets_all$Color)), "/")
+  
+  ##Transform - Vector of unique Color values
+  Color_lvl <- sort(unique(unlist(Color_lst)))
+  
+  ##Transform - Add new data frame columns of unique Color values
+  pets_all <- data.frame(pets_all, do.call(rbind, lapply(Color_lst, function(x) table(factor(x, levels=Color_lvl)))), stringsAsFactors = FALSE)
+  
+  ##Transform - Fill AgeuponOutcome_Years NA values with mean value
+  pets_all[is.na(pets_all$AgeuponOutcome_Years),'AgeuponOutcome_Years'] <- mean(pets_all$AgeuponOutcome_Years, na.rm = TRUE)
 
-##Isolate sex from SexuponOutcome
-pets$SexuponOutcome_Sex <- ifelse(grepl(pattern = " male", tolower(pets$SexuponOutcome)), "male",
-                                  ifelse(grepl(pattern = " female", tolower(pets$SexuponOutcome)), "female",
-                                         "unknown sex") )
+  pets_all <- pets_all[1:pets_rn,]
 
-##Vector of unique sex values
-SexuponOutcome_Sex_lvl <- unique(pets$SexuponOutcome_Sex)
+  return(pets_all)
+}
 
-##Add new data frame columns of unique sex values
-pets <- data.frame(pets, do.call(rbind, lapply(pets$SexuponOutcome_Sex, function(x) table(factor(x, levels=SexuponOutcome_Sex_lvl)))), stringsAsFactors = FALSE)
-
-
-##Isolate neutered from SexuponOutcome
-pets$SexuponOutcome_Neutered <- ifelse(grepl(pattern = "intact", tolower(pets$SexuponOutcome)), "intact",
-                                       ifelse(grepl(pattern = "neutered", tolower(pets$SexuponOutcome)), "neutered",
-                                              ifelse(grepl(pattern = "spayed", tolower(pets$SexuponOutcome)), "neutered",
-                                                     "unknown neutered") ) )
-
-##Vector of unique neutered values
-SexuponOutcome_Neutered_lvl <- unique(pets$SexuponOutcome_Neutered)
-
-##Add new data frame columns of unique neutered values
-pets <- data.frame(pets, do.call(rbind, lapply(pets$SexuponOutcome_Neutered, function(x) table(factor(x, levels=SexuponOutcome_Neutered_lvl)))), stringsAsFactors = FALSE)
-
-##Determine and isolate Mix or Pure breed from Breed
-##If Breed string contains "mix" or "/", then record will be classified as Mix
-pets$Breed_Mix <- as.integer(grepl("mix|\\/", tolower(pets$Breed)))
-pets$Breed_Pure <- as.integer(!pets$Breed_Mix)
-
-##Determine and isolate Mix or Pure color from Color
-#pets$Color_Mix <- grepl("mix|tricolor|torbie|tortie|tabby|seal point|calico|brindle|blue merle|\\/", tolower(pets$Color))
-#pets$Color_Pure <- !pets$Color_Mix
-
-##Scrap distinction of Mix or Pure color
-##To classify Mix or Pure color cannot be done accurately, discretely, and absolutely
-##pets$Color_Mix <- NULL
-##pets$Color_Pure <- NULL
-
-##Vector of unique OutcomeType values
-##OutcomeType_lvl <- unique(pets$OutcomeType)
-
-##Add new data frame columns of unique OutcomeType values
-##pets <- data.frame(pets, do.call(rbind, lapply(pets$OutcomeType, function(x) table(factor(x, levels=OutcomeType_lvl)))), stringsAsFactors = FALSE)
-
-##Vector of unique AnimalType values
-AnimalType_lvl <- unique(pets$AnimalType)
-
-##Add new data frame columns of unique Animal values
-pets <- data.frame(pets, do.call(rbind, lapply(pets$AnimalType, function(x) table(factor(x, levels=AnimalType_lvl)))), stringsAsFactors = FALSE)
-
-##From Breed values, remove "mix" pattern and split combination of breeds that use "/"
-Breed_lst <- strsplit(as.character(sub(" mix", "", tolower(pets$Breed))), "/")
-
-##Vector of unique Breed values
-Breed_lvl <- unique(unlist(Breed_lst))
-
-##Add new data frame columns of unique Breed values
-pets <- data.frame(pets, do.call(rbind, lapply(Breed_lst, function(x) table(factor(x, levels=Breed_lvl)))), stringsAsFactors = FALSE)
-
-##From Color values, split combination of colours that use "/"
-Color_lst <- strsplit(as.character(tolower(pets$Color)), "/")
-
-##Vector of unique Color values
-Color_lvl <- unique(unlist(Color_lst))
-
-##Add new data frame columns of unique Color values
-pets <- data.frame(pets, do.call(rbind, lapply(Color_lst, function(x) table(factor(x, levels=Color_lvl)))), stringsAsFactors = FALSE)
-
-##Fill AgeuponOutcome_Years NA values with mean value
-pets[is.na(pets$AgeuponOutcome_Years),'AgeuponOutcome_Years'] <- mean(pets$AgeuponOutcome_Years, na.rm = TRUE)
+##Transform datasets
+pets <- trans_pets(pets_orig, pets_test_csv)
+pets_test_csv <- trans_pets(pets_test_csv, pets_orig)
 
 ##Visualization
-pets_corr <- pets[,c(4,6,11,12,13,17,22)]
+pets_corr <- pets[,c(4, 6, 11, 12, 15, 19, 22)]
 pets_corr$OutcomeType <- as.integer(pets_corr$OutcomeType)
 pets_corr$AnimalType <- as.integer(pets_corr$AnimalType)
 pets_corr$SexuponOutcome_Sex <- as.integer(as.factor(pets_corr$SexuponOutcome_Sex))
@@ -140,7 +147,8 @@ pets$Color = NULL
 ##Summary statistics
 
 
-##Split pets data into train and test
+##Split pets data (from train.csv) into train and test
+##test.csv cannot be used as outcome classes are not included for competition purposes
 set.seed(555)
 pets_train_rn <- sample(nrow(pets), floor(nrow(pets)*0.7))
 pets_train <- pets[pets_train_rn,]
@@ -156,23 +164,23 @@ classif.task
 ##List learners available to mlr
 #listLearners()
 
-##Trial using classif.multinom -- Multinomial Regression learner
+##Trial - Using classif.multinom -- Multinomial Regression learner
 #rm(list = c("lrn", "mod", "pred", "pets_pred", "perf_classif.multinom"))
-lrn <- makeLearner("classif.multinom", predict.type = "prob", MaxNWts = 1500)
+lrn <- makeLearner("classif.multinom", predict.type = "prob", MaxNWts = 1600)
 mod <- train(lrn, classif.task)
 pred <- predict(mod, newdata = pets_test)
 pets_pred <- pred$data
 perf_classif.multinom <- length(which(pets_pred$truth == pets_pred$response))/nrow(pets_pred)
 
-##Trial using classif.naiveBayes -- Naive Bayes learner
+##Trial - Using classif.naiveBayes -- Naive Bayes learner
 #rm(list = c("lrn", "mod", "pred", "pets_pred", "perf_classif.naiveBayes"))
-lrn <- makeLearner("classif.naiveBayes", predict.type = "prob")
-mod <- train(lrn, classif.task)
-pred <- predict(mod, newdata = pets_test)
-pets_pred <- pred$data
-perf_classif.naiveBayes <- length(which(pets_pred$truth == pets_pred$response))/nrow(pets_pred)
+#lrn <- makeLearner("classif.naiveBayes", predict.type = "prob")
+#mod <- train(lrn, classif.task)
+#pred <- predict(mod, newdata = pets_test)
+#pets_pred <- pred$data
+#perf_classif.naiveBayes <- length(which(pets_pred$truth == pets_pred$response))/nrow(pets_pred)
 
-##Trial using classif.J48 -- J48 Decision Trees learner
+##Trial - Using classif.J48 -- J48 Decision Trees learner
 #rm(list = c("lrn", "mod", "pred", "pets_pred", "perf_classif.J48"))
 lrn <- makeLearner("classif.J48", predict.type = "prob")
 mod <- train(lrn, classif.task)
@@ -180,15 +188,56 @@ pred <- predict(mod, newdata = pets_test)
 pets_pred <- pred$data
 perf_classif.J48 <- length(which(pets_pred$truth == pets_pred$response))/nrow(pets_pred)
 
-##Display performance of multiclass classification learner methods on training dataset
+##Trial - Using classif.rpart -- Decision Tree learner
+#rm(list = c("lrn", "mod", "pred", "pets_pred", "perf_classif.J48"))
+lrn <- makeLearner("classif.rpart", predict.type = "prob")
+mod <- train(lrn, classif.task)
+pred <- predict(mod, newdata = pets_test)
+pets_pred <- pred$data
+perf_classif.rpart <- length(which(pets_pred$truth == pets_pred$response))/nrow(pets_pred)
+
+##Trial - Display performance of multiclass classification learner methods on training dataset
 perf_classif.multinom
-perf_classif.naiveBayes
+#perf_classif.naiveBayes
 perf_classif.J48
+perf_classif.rpart
+
+##Submission - Create task for mlr
+classif.task <- makeClassifTask(data = rbind(pets_train, pets_test), target = "OutcomeType")
+classif.task
+
+##Submission - Using classif.rpart -- Decision Tree learner
+lrn <- makeLearner("classif.rpart", predict.type = "prob")
+mod <- train(lrn, classif.task)
+pred <- predict(mod, newdata = pets_test_csv)
+pets_pred <- pred$data
+submission <- data.frame(pets_test_csv[,1], sapply(levels(pets_pred$response), function(x) as.integer(x == pets_pred$response)))
+names(submission)[1] <- "ID"
+
+##AKC
+pets_dogs_AKC <- read.csv("https://raw.githubusercontent.com/allanreym/ShelterAnimal/master/web_scraped_AKC.csv")
+pets_dogs_AKC$url <- NULL
+pets_dogs_AKC$Personality.Description <- NULL
+pets_dogs_AKC <- pets_dogs_AKC[pets_dogs_AKC$Group != "FSS",]
+
+pets_dogs_AKC$Energy <- droplevels(pets_dogs_AKC$Energy)
+pets_dogs_AKC$Size <- droplevels(pets_dogs_AKC$Size)
+pets_dogs_AKC$Group <- droplevels(pets_dogs_AKC$Group)
+pets_dogs_AKC$Breed <- droplevels(pets_dogs_AKC$Breed)
+
+pets_dogs_AKC$Energy <- ordered(pets_dogs_AKC$Energy, levels(pets_dogs_AKC$Energy)[c(2, 3, 1)])
+head(pets_dogs_AKC$Energy)
+
+pets_dogs_AKC$Size <- ordered(pets_dogs_AKC$Size, levels(pets_dogs_AKC$Size)[c(3, 2, 1)])
+head(pets_dogs_AKC$Size)
+
+str(pets_dogs_AKC)
 
 
+pets_dogs_Petfinder <- read.csv("https://raw.githubusercontent.com/allanreym/ShelterAnimal/master/web_scraped_Petfinder.csv")
 
 
 #######################
-
-# submission <- data.frame(pets_test_pred[,1], sapply(levels(pets_test_pred$response), function(x) as.integer(x == pets_test_pred$response)))
-#                          table(factor(submission, levels = levels(submission$response)))
+##Old submission
+#submission <- data.frame(pets_test_pred[,1], sapply(levels(pets_test_pred$response), function(x) as.integer(x == pets_test_pred$response)))
+#                         table(factor(submission, levels = levels(submission$response)))
